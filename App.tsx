9a8013872d91e67c395e39eec9961eb7e5d5b7dc
nandobfer <nandobfer@yahoo.com.br>
2023-11-03 16:18:59 -0300

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: "red" }}>
+            <WebView source={{ uri: "https://gymagochi.nandoburgos.dev" }} textZoom={100} originWhitelist={["*"]} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
+
+export default App
